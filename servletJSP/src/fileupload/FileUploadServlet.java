66cc2fbf4dcd77dc4a182af3a324411cfe7a3616
peter <iampeter@home.com>
2021-08-13 16:41:13 +0900

    d

--- servletJSP/src/fileupload/FileUploadServlet.java
+++ servletJSP/src/fileupload/FileUploadServlet.java
@@ -13,29 +13,17 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.Part;
 
-/**
- * Servlet implementation class FileUploadServlet
- */
 @MultipartConfig(location = "C:\\tmp", maxFileSize = -1, maxRequestSize = -1, fileSizeThreshold = 1024)
 @WebServlet("/fileUpload")
 public class FileUploadServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-	/**
-	 * @see HttpServlet#HttpServlet()
-	 */
 	public FileUploadServlet() {
 		super();
-		// TODO Auto-generated constructor stub
 	}
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
-	 *      response)
-	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		// TODO Auto-generated method stub
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
