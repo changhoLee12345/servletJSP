341ebaef63cc4fa9671320f7f936fc516dbe1e71
leadon <leadon@daum.net>
2022-01-13 04:56:46 +0900

    d

--- servletJSP/src/com/edu/test/filter/FlowFilterThree.java
+++ servletJSP/src/com/edu/test/filter/FlowFilterThree.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
@@ -26,6 +27,13 @@ public class FlowFilterThree implements Filter {
 	@Override
 	public void destroy() {
 		// TODO Auto-generated method stub
-		Filter.super.destroy();
+		
 	}
+
+	@Override
+	public void init(FilterConfig filterConfig) throws ServletException {
+		// TODO Auto-generated method stub
+		
+	}
+
 }
