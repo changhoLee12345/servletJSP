ae13a7fd0b17214f655ad4d3387b1b97c45a9e83
new <new@email.com>
2022-07-27 20:35:14 +0900

    fd

--- servletJSP/src/com/dev/controller/HttpUtil.java
+++ servletJSP/src/com/dev/controller/HttpUtil.java
@@ -1,6 +1,7 @@
 package com.dev.controller;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -14,6 +15,15 @@ public class HttpUtil {
 		try {
 			rd.forward(request, response);
 		} catch (ServletException | IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void getJson(HttpServletResponse resp, Object jsonObj) {
+		try {
+			PrintWriter out = resp.getWriter();
+			out.print(jsonObj);
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
